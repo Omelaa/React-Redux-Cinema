3b7335ca6711d8f9206583b831800d3cdeeeb6fb
Omelaa <Ostpa28@gmail.com>
2022-07-29 21:31:54 +0300

    add scss and reset styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import css from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className={css.container}>
+        <h1>hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
